769d5b867c1b469ebbfafb53286e218bd41470d3
aminDB81 <aminbaluchi67@gmail.com>
2023-05-12 19:22:12 +0330

    Add footer Section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 // get  Get Free Quote window
 var modal = document.getElementById('myModal')
 var btn = document.getElementById('MyBtn')
@@ -10,50 +9,47 @@ btn.onclick = function () {
 span.onclick = function () {
   modal.style.display = 'none'
 }
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none'
   }
+}
 
+// Header fix
+window.addEventListener('scroll', function () {
+  var header = document.getElementById('myHeader')
+  if (window.pageYOffset > 0) {
+    header.classList.add('scrolled')
+  } else {
+    header.classList.remove('scrolled')
+  }
+})
 
-  // Header fix
-  window.addEventListener("scroll", function() {
-    var header = document.getElementById("myHeader");
-    if (window.pageYOffset > 0) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
-  });
-
-
-
-  // container
-const targetElement = document.getElementById("conter-area");
+// container
+const targetElement = document.getElementById('conter-area')
 // Create the intersection observer
 const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     // If the target element is in view
     if (entry.isIntersecting) {
-      let valueDisplays = document.querySelectorAll(".num");
-      let interval = 7000;
+      let valueDisplays = document.querySelectorAll('.num')
+      let interval = 7000
       valueDisplays.forEach((valueDisplays) => {
-        let statrtValue = 0;
-        let endValue = parseInt(valueDisplays.getAttribute("data-val"));
-        let duration = Math.floor(interval / endValue);
-        let counter = setInterval(function(){
-          statrtValue += 1;
-          valueDisplays.textContent = statrtValue;
-          if (statrtValue == endValue){
-            clearInterval(counter);
+        let statrtValue = 0
+        let endValue = parseInt(valueDisplays.getAttribute('data-val'))
+        let duration = Math.floor(interval / endValue)
+        let counter = setInterval(function () {
+          statrtValue += 1
+          valueDisplays.textContent = statrtValue
+          if (statrtValue == endValue) {
+            clearInterval(counter)
           }
-        }, duration);
-      });
+        }, duration)
+      })
       // Stop observing once the element is in view
-      observer.unobserve(targetElement);
+      observer.unobserve(targetElement)
     }
-  });
-});
+  })
+})
 // Start observing the target element
-observer.observe(targetElement);
+observer.observe(targetElement)
